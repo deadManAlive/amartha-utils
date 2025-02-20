f52e6404c930a5c496f300c63e540d6c00b79a4a
deadManAlive <alfarrakhan@gmail.com>
2025-02-21 06:22:07 +0700

    some changes

--- utils.go
+++ utils.go
@@ -2,6 +2,27 @@ package utils
 
 import "fmt"
 
-func PrintSlice([]int) {
-	fmt.Println("Test")
+func PrintSlice(s []int) {
+	for _, v := range s {
+		fmt.Printf("%4d ", v)
+	}
+	fmt.Println()
+}
+
+func PrintSlice2(s [][]int) {
+	for _, v := range s {
+		for _, w := range v {
+			fmt.Printf("%4d ", w)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func SqEmpMatGen(n int) [][]int {
+	mat := make([][]int, n)
+	for i := range n {
+		mat[i] = make([]int, n)
+	}
+	return mat
 }
